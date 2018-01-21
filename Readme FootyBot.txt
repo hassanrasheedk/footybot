d917a2cff92d8cdd66f1df86607922844dccfed8
Hassan Rasheed <hassan.rasheed8@gmail.com>
2018-01-22 00:20:33 +0500

    Add files via upload

---
+++ Readme FootyBot.txt
@@ -0,0 +1,30 @@
+Project Title 
+---Footy Bot---
+A robot that plays football.
+In this project the robot finds the ball and puts it into the goal, 
+it does this with the help of OpenCV (Open source computer vision library) 
+using a webcam and an arduino uno which have been programmed to perform this task. 
+
+------GROUP MEMBERS------
+
+Hassan Rasheed 126900 BSCS5B
+Saad Sohail 126524 BSCS5B
+Muhammad Ahsan Fahim 129416 BSCS5B
+
+
+------INSTRUCTIONS------
+
+Following is a step by step procedure on how to make the code work:
+
+1. Include OpenCV 3.1
+   Which can be downloaded at the following link.
+   http://opencv.org/downloads.html
+
+   Following link shows how to setup opencv 3.1 on visual studio 2015
+   https://www.youtube.com/watch?v=l4372qtZ4dc
+
+2. Include the Serial directory placed in the project folder.
+
+3. Arduino's code runs on Arduino's IDE which can be downloaded from the following link.
+   https://www.arduino.cc/en/main/software
+
---
+++ arduinofinalcode/arduinofinalcode.ino
@@ -0,0 +1,58 @@
+int incomingByte = 0;   // for incoming serial data
+int motor1 = 3;
+int motor2=9;
+void setup()
+{
+  Serial.begin(9600);          // opens serial port, sets data rate to 9600 bps
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
+}
+ 
+void loop()
+{ 
+  digitalWrite(motor1, LOW);        // turn the motors off by making the voltage LOW
+  digitalWrite(motor2, LOW);
+ 
+  if (Serial.available() > 0) 
+  {
+    // read the incoming byte:
+    incomingByte = Serial.read();         //received data
+  }
+
+  if(incomingByte=='B'||incomingByte==0)
+  {
+    digitalWrite(motor1, LOW);        // turn the motors off by making the voltage LOW
+    digitalWrite(motor2, LOW);
+    delay(400);
+    incomingByte=0;
+  }
+
+  if(incomingByte=='F')            //going straight so both motors spinning
+  {
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,HIGH);
+    delay(50);
+    incomingByte=0;
+  }
+  if(incomingByte=='R')         //going right
+  {
+    digitalWrite(motor1,HIGH);
+    digitalWrite(motor2,LOW);
+    delay(50);
+    incomingByte=0;
+  }
+
+  if(incomingByte=='L')         //going left
+  {
+    digitalWrite(motor2,HIGH);
+    digitalWrite(motor1,LOW);
+    delay(50);
+    incomingByte=0;
+  }
+  if(incomingByte=='E')
+  {
+    delay(50);
+    incomingByte=0;
+  }
+  }
+
